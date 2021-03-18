bfc3905b54319d6b69244418618f054f502b0fcb
Ivan Ferenčak <iferencak@profico.hr>
2021-03-19 00:03:53 +0100

    Initial Commit

---
+++ PFPageControl/PFPageControl.h
@@ -0,0 +1,18 @@
+//
+//  PFPageControl.h
+//  PFPageControl
+//
+//  Created by Ivan Ferencak on 19.03.2021..
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PFPageControl.
+FOUNDATION_EXPORT double PFPageControlVersionNumber;
+
+//! Project version string for PFPageControl.
+FOUNDATION_EXPORT const unsigned char PFPageControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PFPageControl/PublicHeader.h>
+
+
